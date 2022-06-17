ab0a10896828d7adb286c9946d035dd39f262b3c
PipZip <37911683+PipZip@users.noreply.github.com>
2022-06-17 21:43:38 +0600

    update

--- src/app.js
+++ src/app.js
@@ -3,13 +3,7 @@
 // Styles
 import 'styles/_app.scss'
 
-$(function() {
-	console.log('Ready!')
-	require('scripts/aos')
+require('scripts/slick')
 
-	require('scripts/slick')
-
-	require('scripts/main')
-	require('scripts/tabs')
-	require('scripts/header')
-})
+require('scripts/main')
+require('scripts/header')
